522419415f1ddd9db3f333e0208713d73239ce4d
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2022-07-30 00:04:45 +0530

    patch/ScoreBoard passed tests, README.md

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Classes
 
-I am creating 2 classes: Game and ScoreBoard.
+I am creating 2 classes: Game and ScoreBoard following (S: single responsibility) in SOLID principle.
+
 Game having homeTeam, awayTeam, homeScore, awayScore, and startingTime ( to sort matches by time added to the system). Game also have constructor, getters, and setters.
 ScoreBoard having games as an ArrayList insead of Array for dynamic allocation. It will also have methods for startGame, updateScores, finsihGame, orderGames, and getSummary.
 
@@ -10,6 +11,5 @@ I am also creating a main class to run the application.
 
 I am following TDD approach, writing the tests first. Giving an Input, expecting an output, modifying the code until getting the desired results.
 
-
-ScoreBoard is public class (outer)
-Game is no-keyword class (package-private)
+ScoreBoard is public class (outer).
+Game is no-keyword class (package-private).
--- ScoreBoard.java
+++ ScoreBoard.java
@@ -30,4 +30,44 @@ public class ScoreBoard {
         }
     }
 
+    void finishGame(String homeTeam, String awayTeam) {
+        for (int i = 0; i < games.size(); i++) {
+            if (games.get(i).getHomeTeam().equals(homeTeam) && games.get(i).getAwayTeam().equals(awayTeam)) {
+                games.remove(i);
+            }
+        }
+    }
+
+    void orderGames() {
+        for (int i = 0; i < games.size(); i++) {
+            for (int j = i + 1; j < games.size(); j++) {
+                if (games.get(i).getHomeScore() + games.get(i).getAwayScore() < games.get(j).getHomeScore()
+                        + games.get(j).getAwayScore()) {
+                    Game temp = games.get(i);
+                    games.set(i, games.get(j));
+                    games.set(j, temp);
+                } else if (games.get(i).getHomeScore() + games.get(i).getAwayScore() == games.get(j).getHomeScore()
+                        + games.get(j).getAwayScore()) {
+                    if (games.get(i).getStartingTime() < games.get(j).getStartingTime()) {
+                        Game temp = games.get(i);
+                        games.set(i, games.get(j));
+                        games.set(j, temp);
+                    }
+                }
+
+            }
+        }
+    }
+
+    String getSummary() {
+        orderGames();
+        String summary = "";
+        for (int i = 0; i < games.size(); i++) {
+            summary += games.get(i).getHomeTeam() + " " + games.get(i).getHomeScore() + " - "
+                    + games.get(i).getAwayTeam()
+                    + " " + games.get(i).getAwayScore() + "\n";
+        }
+        return summary;
+    }
+
 }
