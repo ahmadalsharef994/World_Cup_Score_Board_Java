ce4c5b7a7b37a33d2e585b469b7ac8bc4c1a2c30
ahmadalsharef994 <ahmadalsharef2021@gmail.com>
2022-07-29 19:36:04 +0530

    ScoreBoard class TDD tests: given input --> given output

--- main.java
+++ main.java
@@ -28,5 +28,25 @@ public class main {
         scoreBoardTest.updateScores("home", "away", 1, 1);
         assertEquals(1, scoreBoardTest.getGames().get(0).getHomeScore());
 
+        // 3. TDD for class ScoreBoard expecting given input
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.startGame("Mexico", "Canada");
+        scoreBoard.startGame("Spain", "Brazil");
+        scoreBoard.startGame("Germany", "France");
+        scoreBoard.startGame("Uruguay", "Italy");
+        scoreBoard.startGame("Argentina", "Australia");
+        scoreBoard.updateScores("Mexico", "Canada", 0, 5);
+        scoreBoard.updateScores("Spain", "Brazil", 10, 2);
+        scoreBoard.updateScores("Germany", "France", 2, 2);
+        scoreBoard.updateScores("Uruguay", "Italy", 6, 6);
+        scoreBoard.updateScores("Argentina", "Australia", 3, 1);
+        assertEquals(
+                "Uruguay 6 - Italy 6\nSpain 10 - Brazil 2\nMexico 0 - Canada 5\nArgentina 3 - Australia 1\nGermany 2 - France 2\n",
+                scoreBoard.getSummary());
+        scoreBoard.finishGame("Mexico", "Canada");
+        assertEquals("Uruguay 6 - Italy 6\nSpain 10 - Brazil 2\nArgentina 3 - Australia 1\nGermany 2 - France 2\n",
+                scoreBoard.getSummary());
+        scoreBoard.finishGame("Uruguay", "Italy");
+        assertEquals("Spain 10 - Brazil 2\nArgentina 3 - Australia 1\nGermany 2 - France 2\n", scoreBoard.getSummary());
     }
 }
